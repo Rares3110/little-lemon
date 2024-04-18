db416d2f4aa900ae0c5841f5628c3089067539bd
Rares Popa <rares_teodor@yahoo.ro>
2024-04-19 00:20:10 +0300

    more unit tests

--- src/tests/BookingForm.test.jsx
+++ src/tests/BookingForm.test.jsx
@@ -1,10 +1,19 @@
 import { screen, render } from "@testing-library/react";
 import { expect, test } from "vitest";
 import BookingForm from '../components/BookingPage/BookingForm';
+import BookingMain from "../components/BookingPage/BookingMain";
 import { defaultInfo, availableTimes } from "../components/BookingPage/BookingMain";
+import { fireEvent } from "@testing-library/react";
 
-test('Renders the BookingForm labels', () => {
+test('Renders the BookingForm label', () => {
     render(<BookingForm info={defaultInfo} setInfo={() => {}} availableTimes={availableTimes} />);
-    const labelElement = screen.getByText("Choose date");
-    expect(labelElement).toBeInTheDocument();
+    const label = screen.getByText("Choose date");
+    expect(label).toBeInTheDocument();
+})
+
+test('Change time in the BookingForm', () => {
+    const { container } = render(<BookingMain/>);
+    const timeSelect = container.querySelector("select#res-time");
+    fireEvent.change(timeSelect, { target: { value: availableTimes[2] } });
+    expect(timeSelect).toHaveValue(availableTimes[2]);
 })
